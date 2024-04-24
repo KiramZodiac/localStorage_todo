67376d040be8e7a9ce69817f58f829e93fa6679d
kiramZodiac <kiramzodiac@gmail.com>
2024-04-24 13:35:26 +0300

    added and cleaned up the code plus re designing the face book clone

--- toDo.js
+++ toDo.js
@@ -9,9 +9,6 @@ const ret = document.getElementById('ret')
 let delBtn = document.createElement('button')
 delBtn.innerHTML = 'delete'
 
-
-
-
 btn.addEventListener('click',()=>{
 
 if(input.value !==""){
@@ -21,38 +18,29 @@ if(input.value !==""){
     
     lis.append(input.value)
     
-    li.append(lis,delBtn)
+    li.appendChild(lis)
     
 
 saveTask()
-
-
     input.value = ''
+
 }else{
 
     alert('please enter a to do')
 }
-
-
 })
-
-
 function saveTask(){
 const taskList = document.getElementById('li')
 
 const tasks =[]
+
 taskList.querySelectorAll('li').forEach(function(taskItem){
     tasks.push(taskItem.textContent)
-localStorage.setItem('tasks',JSON.stringify(tasks,delBtn))
+localStorage.setItem('tasks',JSON.stringify(tasks))
 
 
 })
 }
-
-
-
-
-
 function retTask(){
 
     const taskList = document.getElementById('li')
@@ -65,7 +53,7 @@ tsk.forEach(function(taskText){
     const li = document.createElement('li');
     li.textContent = taskText
     
-    taskList.appendChild(li,delBtn)
+    taskList.appendChild(li)
     
 })
 
@@ -81,8 +69,10 @@ setTimeout(()=>{
 
 }
 
-  
-delBtn.addEventListener('click',()=>{
 
-localStorage.clear()
+
+li.addEventListener('click', () => {
+    li.forEach(element => {
+       element.remove() 
+    });
 })
